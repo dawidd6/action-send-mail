445b6879accb203777ef4592c6c98ab00db25157
Dawid Dziurla <dawidd0811@gmail.com>
2019-12-06 10:18:17 +0100

    main: format

--- main.rb
+++ main.rb
@@ -4,29 +4,29 @@
 require 'net/smtp'
 
 # Inputs
-address = ENV["INPUT_SERVER_ADDRESS"]
-port = ENV["INPUT_SERVER_PORT"]
-username = ENV["INPUT_USERNAME"]
-password = ENV["INPUT_PASSWORD"]
-subject = ENV["INPUT_SUBJECT"]
-body = ENV["INPUT_BODY"]
-to = ENV["INPUT_TO"]
-from = ENV["INPUT_FROM"]
+address = ENV['INPUT_SERVER_ADDRESS']
+port = ENV['INPUT_SERVER_PORT']
+username = ENV['INPUT_USERNAME']
+password = ENV['INPUT_PASSWORD']
+subject = ENV['INPUT_SUBJECT']
+body = ENV['INPUT_BODY']
+to = ENV['INPUT_TO']
+from = ENV['INPUT_FROM']
 
 # Message
-message = <<EOM
-Subject: #{subject}
-From: #{from} <#{username}>
+message = <<~END_OF_MESSAGE
+  Subject: #{subject}
+  From: #{from} <#{username}>
 
-#{body}
-EOM
+  #{body}
+END_OF_MESSAGE
 
 # Send
 begin
   smtp = Net::SMTP.new(address, port.to_i)
   smtp.enable_tls
   smtp.start(address, username, password, :login)
-  smtp.send_message(message, username, to.split(","))
+  smtp.send_message(message, username, to.split(','))
 rescue StandardError => e
   puts "Error: #{e.message}"
   exit 1
