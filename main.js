bf9201fd60f82f3f429b32b3e4e982f8ae35d19b
dougpagani <redstonefreedom@gmail.com>
2020-11-30 04:05:37 -0500

    Add markdown conversion to code

--- main.js
+++ main.js
@@ -1,14 +1,31 @@
 const nodemailer = require("nodemailer")
 const core = require("@actions/core")
 const fs = require("fs")
+const showdown = require('showdown')
 
-function getBody(body) {
-    if (body.startsWith("file://")) {
-        const file = body.replace("file://", "")
-        return fs.readFileSync(file, "utf8")
+function getBody(bodyOrFile, convert_to_html_from_markdown) {
+    let unconvertedBody
+    if (bodyOrFile.startsWith("file://")) {
+        const file = bodyOrFile.replace("file://", "")
+        unconvertedBody = fs.readFileSync(file, "utf8")
+    } else {
+      unconvertedBody = bodyOrFile
+    }
+
+    let body
+    if (convert_to_html_from_markdown === "true") {
+      body = convertFromMarkdownToHTML(unconvertedBody)
+    } else {
+      body = unconvertedBody
     }
 
     return body
+}
+
+function convertFromMarkdownToHTML(markdownString) {
+  let converter = new showdown.Converter()
+  let htmlString = converter.makeHtml(markdownString) 
+  return htmlString
 }
 
 function getFrom(from, username) {
@@ -31,6 +48,7 @@ async function main() {
         const from = core.getInput("from", { required: true })
         const content_type = core.getInput("content_type", { required: true })
         const attachments = core.getInput("attachments", { required: false })
+        const convert_to_html_from_markdown = core.getInput("convert_to_html_from_markdown", { required: false })
 
         const transport = nodemailer.createTransport({
             host: server_address,
@@ -48,8 +66,8 @@ async function main() {
             from: getFrom(from, username),
             to: to,
             subject: subject,
-            text: content_type != "text/html" ? getBody(body) : undefined,
-            html: content_type == "text/html" ? getBody(body) : undefined,
+            text: content_type != "text/html" ? getBody(body, convert_to_html_from_markdown) : undefined,
+            html: content_type == "text/html" ? getBody(body, convert_to_html_from_markdown) : undefined,
             attachments: attachments ? attachments.split(',').map(f => ({ path: f.trim() })) : undefined
         })
 
