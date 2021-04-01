edbf8304f7dcbcf3f14b81f35281551b9dd894ba
Adam Dobrawy <naczelnik@jawnosc.tk>
2021-04-02 01:31:04 +0200

    Skip peerDependencies

--- node_modules/@react-native-community/cli-platform-android/build/config/readManifest.d.ts
+++
@@ -1,3 +0,0 @@
-import xml from 'xmldoc';
-export default function readManifest(manifestPath: string): xml.XmlDocument;
-//# sourceMappingURL=readManifest.d.ts.map
--- node_modules/@react-native-community/cli-platform-android/build/utils/getAndroidProject.d.ts
+++
@@ -1,8 +0,0 @@
-import { Config, AndroidProjectConfig } from '@react-native-community/cli-types';
-export declare function getAndroidProject(config: Config): AndroidProjectConfig;
-/**
- * Get the package name of the running React Native app
- * @param config
- */
-export declare function getPackageName(androidProject: AndroidProjectConfig, appFolder?: string): string;
-//# sourceMappingURL=getAndroidProject.d.ts.map
