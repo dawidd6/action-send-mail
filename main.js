28a74a279e0b0050e97876a53a2150fbba255c3e
Dawid Dziurla <dawidd0811@gmail.com>
2021-04-25 16:02:37 +0200

    Update main.js

--- main.js
+++ main.js
@@ -36,13 +36,13 @@ async function main() {
         const username = core.getInput("username", { required: true })
         const password = core.getInput("password", { required: true })
         const subject = core.getInput("subject", { required: true })
-        const body = core.getInput("body", { required: true })
         const from = core.getInput("from", { required: true })
         const to = core.getInput("to", { required: true })
+        const body = core.getInput("body", { required: false })
+        const htmlBody = core.getInput("html_body", { required: false })
         const cc = core.getInput("cc", { required: false })
         const bcc = core.getInput("bcc", { required: false })
         const replyTo = core.getInput("reply_to", { required: false })
-        const contentType = core.getInput("content_type", { required: true })
         const attachments = core.getInput("attachments", { required: false })
         const convertMarkdown = core.getInput("convert_markdown", { required: false })
         const ignoreCert = core.getInput("ignore_cert", { required: false })
@@ -59,33 +59,16 @@ async function main() {
                 rejectUnauthorized: false
             } : undefined
         })
-
-        let htmlPart = undefined;
-        let plainPart = undefined;
-        switch (contentType) {
-            case 'text/html':
-                htmlPart = getBody(body, convertMarkdown);
-                break;
-            case 'text/plain':
-                plainPart = getBody(body, convertMarkdown);
-                break;
-            case 'multipart/alternative':
-                plainPart = getBody(body, convertMarkdown);
-                const html_body = core.getInput("html_body", { required: false })
-                htmlPart = getBody(html_body, convertMarkdown);
-                break;
-            default:
-                throw `invalid content_type: ${contentType}`;
-        }
+        
         const info = await transport.sendMail({
             from: getFrom(from, username),
             to: to,
+            subject: subject,
             cc: cc ? cc : undefined,
             bcc: bcc ? bcc : undefined,
             replyTo: replyTo ? replyTo : undefined,
-            subject: subject,
-            text: plainPart,
-            html: htmlPart,
+            text: body ? getBody(body, convertMarkdown) : undefined,
+            html: htmlBody ? getBody(htmlBody, convertMarkdown) : undefined,
             attachments: attachments ? attachments.split(',').map(f => ({ path: f.trim() })) : undefined
         })
     } catch (error) {
