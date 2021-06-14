ca195102431428a4bed945db514345ba3a9a6638
Dawid Dziurla <dawidd0811@gmail.com>
2021-06-14 20:02:17 +0200

    Update main.js

--- main.js
+++ main.js
@@ -59,8 +59,8 @@ async function main() {
                 pass: password
             } : undefined,
             port: serverPort,
-            secure: secure === "true" ? true : serverPort == "465",
-            tls: ignoreCert === "true" ? {
+            secure: secure == "true" ? true : serverPort == "465",
+            tls: ignoreCert == "true" ? {
                 rejectUnauthorized: false
             } : undefined,
         })
